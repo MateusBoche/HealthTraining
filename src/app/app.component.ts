cb2274e1d921bba2b301464b33fc5e6201b04295
Mateus Boche <mateusboche@gmail.com>
2024-05-09 19:36:00 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'lds-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'lds-frontend';
+}
