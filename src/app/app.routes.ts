d3d150e181630aeee38e14d5ec3c24b6e92e4cb8
Mateus Boche <mateusboche@gmail.com>
2024-05-09 20:48:01 -0300

    feat: adicao da rota para a pagina de sign-in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {SignInComponent} from "./views/accout/sign-in/sign-in.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{
+  path: "account/sign-in",
+  component: SignInComponent
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+];
