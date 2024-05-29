553f5f5dc22dbeef9cb4526d2e1f76322555c5d1
Mateus Boche <mateussenaisrs@gmail.com>
2024-05-28 22:50:34 -0300

    feat: implementação do product list

--- src/app/views/app/product-list/product-list.component.ts
+++ src/app/views/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import * as fontawesome from '@fortawesome/free-solid-svg-icons';
 
@@ -7,6 +8,7 @@ import * as fontawesome from '@fortawesome/free-solid-svg-icons';
   standalone: true,
   imports: [
     FontAwesomeModule,
+    RouterModule
   ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
@@ -14,4 +16,36 @@ import * as fontawesome from '@fortawesome/free-solid-svg-icons';
 export class ProductListComponent {
   fa = fontawesome;
 
+  products: Products[] = [
+    {
+      id: 1,
+      name: 'produto 1',
+      price: 44.54
+
+    },
+    {
+      id: 2,
+      name: 'produto 2',
+      price: 58.74
+
+    },
+    {
+      id: 3,
+      name: 'produto 3',
+      price: 64.20
+
+    },
+    {
+      id: 4,
+      name: 'produto 4',
+      price: 100.41
+
+    },
+  ];
+}
+
+export interface Products{
+  id: number;
+  name: String;
+  price:number;
 }
