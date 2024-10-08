866096a3ef62ad9bb8f5cfb22ae26533245618f6
Mateus Boche <mateussenaisrs@gmail.com>
2024-10-08 08:19:52 -0300

    feat: ajustando authentiation service

--- frontend/pi_health_training/src/app/services/security/guard/authentication.guard.ts
+++ frontend/pi_health_training/src/app/services/security/guard/authentication.guard.ts
@@ -1,21 +1,22 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthenticationService } from '../../authentication.service';
+import { AuthenticationService } from '../authentication.service';
 
-export const authenticationGuard: 
-CanActivateFn = (route, state) => {
-  
+export const authenticationGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const authenticationService = inject(AuthenticationService);
 
+  // const isAuthenticated = false;
   const isAuthenticated = authenticationService.isAuthenticated();
 
-  if(isAuthenticated) {
+  console.log(router.url);
+
+  if (isAuthenticated) {
     return true;
   }
 
-  if(router.url === 'account/sign-up') {
-    router.navigate(['account/sign-in']);
+  if (router.url === 'account/sign-up') {
+    router.navigate(['account/sign-up']);
     return false;
   }
 
