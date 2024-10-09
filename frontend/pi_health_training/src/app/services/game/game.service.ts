536dffe0ddfb0b526e031283b1d83c14b77499b4
Marcos <marcosrodrigues-srs1@hotmail.com>
2024-10-09 16:12:58 -0300

    feat: implementacao do game service

--- frontend/pi_health_training/src/app/services/game/game.service.ts
+++ frontend/pi_health_training/src/app/services/game/game.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Game } from '../../domain/model/game';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  findById(id: string): Promise<Game> {
+    return firstValueFrom(this.http.get<Game>(`${this.apiUrl}/game/${id}`));
+  }
+
+  findAllQuestions(): Promise<{
+    question: string,
+    answer: boolean,
+    category: string,
+    id: string,
+    phase: number
+  }[]> {
+    return firstValueFrom(this.http.get<{
+      question: string,
+      answer: boolean,
+      category: string,
+      id: string,
+      phase: number
+    }[]>(`${this.apiUrl}/questions`));
+  }
+
+  saveGameState(id: string, gameState: any): Promise<any> {
+    return firstValueFrom(this.http.put(`${this.apiUrl}/game/${id}`, gameState));
+  }
 }
